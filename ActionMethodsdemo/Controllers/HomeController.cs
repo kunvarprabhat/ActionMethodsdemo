620b0ac57ba9e9ca9fda9fe2e51a0672647343a1
kunvar prabhat <kunvarprabhat44@gmail.com>
2023-01-06 18:01:33 +0530

    Add project files.

---
+++ ActionMethodsdemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionMethodsdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Show()
+        {
+            return "This is the a second action method of Home Controller";
+        } 
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public int StudentID(int id)
+        {
+            return id;
+        }
+    }
+}
